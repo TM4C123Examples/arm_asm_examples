15e117d9ee5169b226b0572b0ccb16648d99648e
Manuel Limones <limonesu.me@gmail.com>
2016-09-10 09:41:15 -0500

    Add code description to example 07

--- asm_example_07/source/main.s
+++ asm_example_07/source/main.s
@@ -5,33 +5,33 @@ main    PROC ;start of function main
 
         MOV R0,#10
         MOV R1,#2
-        MOV R2,#0
 
-        ;while loop
+        ;This example calculates the division R2 = R0/R1 using subtractions and a while loop
+        MOV R2,#0
         B while_compare
 while_block
-        SUB R0,R1
-        ADD R2,#1
+        SUB R0,R1; R0 = R0 - R1
+        ADD R2,#1; R2++
 while_compare
         CMP R0,#0
-        BGT while_block
-
+        BGT while_block; while(R1 > 0)
 
         ;for loop
-
-        MOV R0,#3
+        ;This example caculates the sum over n from n = 0 to n = R0
+        ;R1 is used as counter "n" and R2 is used as acumulator "accu"
+        MOV R0,#4; R0 = 4
 
 for_init
-        MOV R1,#0; init counter counter
-        MOV R2,#0; init local accumulator
+        MOV R1,#0; init local counter, n = 0
+        MOV R2,#0; init local accumulator, accu = 0
         B for_compare
 for_block
-        ADD R2,R1
+        ADD R2,R1; accu = accu + n;
 for_inc
-        ADD R1,#1;increase counter
+        ADD R1,#1; n++
 for_compare
         CMP R1,R0
-        BLE for_block
+        BLE for_block;for(n = 0; n <= R0; n++)
 
         B .; while(1);
 
