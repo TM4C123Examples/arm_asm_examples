e75e978faedc8f53635f7ee8d40ea11bfd7f4559
Manuel Limones <limonesu.me@gmail.com>
2016-09-10 08:56:39 -0500

    Add code description to example 05 code

--- asm_example_05/source/main.s
+++ asm_example_05/source/main.s
@@ -3,20 +3,25 @@
 main    PROC ;start of function main
         EXPORT  main
 
+        ;Relational operators and conditional execution
+
         MOV R0,#0x02
         MOV R1,#0x03
         MOV R2,#0x00
 
-        CMP R0,R1;
-        MOVGT R2,#0x0A
+        CMP R0,R1;the CMP (compare) operator executes a SUB operation (R0 - R1) and the result discarded
+        ;After CMP the instruccion flags are updated
+        ;Most instrucctions can be executed conditionally, if the condition is not meet the instruccion is ignored (skiped)
+        MOVGT R2,#0x0A; execute MOV + GT execute only if the relation of last compare is grater than, (R0 > R1)
 
-        MOV R2,#0x00
-        CMP R1,R0;
-        MOVGT R2,#0x0A
 
-        MOV R0,#0x00
-        CMP R0,R1;
+        ;In this example the jump will not happen since the condition is satisfied
+        MOV R2,#0x00; reset R2 register
+        CMP R1,R0;compare R1 R0
+        MOVGT R2,#0x0A; excute MOV only if (R1 > R0)
 
+        ;Other relational condition are available
+        CMP R0,R1;Compare R0 and R1
         MOVGT R3,#0x11; R0 > R1
         MOVGE R3,#0x12; R0 >= R1
         MOVLT R3,#0x13; R0 < R1
