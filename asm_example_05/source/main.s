cf31dc195025191ef620770e5eaec69ac5592ba3
Manuel Limones <limonesu.me@gmail.com>
2016-09-09 23:01:41 -0500

    Remove trailing spaces

--- asm_example_05/source/main.s
+++ asm_example_05/source/main.s
@@ -6,17 +6,17 @@ main    PROC ;start of function main
         MOV R0,#0x02
         MOV R1,#0x03
         MOV R2,#0x00
-        
+
         CMP R0,R1;
         MOVGT R2,#0x0A
 
         MOV R2,#0x00
         CMP R1,R0;
         MOVGT R2,#0x0A
-        
+
         MOV R0,#0x00
         CMP R0,R1;
-        
+
         MOVGT R3,#0x11; R0 > R1
         MOVGE R3,#0x12; R0 >= R1
         MOVLT R3,#0x13; R0 < R1
@@ -25,7 +25,7 @@ main    PROC ;start of function main
         MOVNE R3,#0x16; R0 != R1
         MOVMI R3,#0x17; (R0 - R1) < 0
         MOVPL R3,#0x18; (R0 - R1) >= 0
-        
+
         B .; while(1);
 
         ENDP; End of function main
