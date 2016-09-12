260aedb0eb529c90d3fb2ad5ca5d0d2a6450d3c3
Manuel Limones <limonesu.me@gmail.com>
2016-09-12 13:04:17 -0500

    Add base project for example 10

---
+++ asm_example_10/source/main.s
@@ -0,0 +1,10 @@
+        AREA    |.text|, CODE, READONLY
+
+main    PROC ;start of function main
+        EXPORT  main
+
+        B .; while(1);
+
+        ENDP; End of function main
+
+        END ;End of file
